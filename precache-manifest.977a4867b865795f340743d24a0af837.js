0d118e570f394c1e2188630381e62389292d7c32
peter.georgiev <c00p3r4o+>
2019-11-27 11:49:48 +0100

    Modifying

--- precache-manifest.977a4867b865795f340743d24a0af837.js
+++ precache-manifest.977a4867b865795f340743d24a0af837.js
@@ -1,16 +1,16 @@
 self.__precacheManifest = [
   {
-    "url": "6.6cef46167909bc46742b.bundle.js"
+    "url": "6.d2f11f9a11ecf845e981.bundle.js"
   },
   {
-    "url": "login.6cef46167909bc46742b.bundle.js"
+    "url": "login.d2f11f9a11ecf845e981.bundle.js"
   },
   {
-    "revision": "f837413d76aea2e01de5",
+    "revision": "9c68b4d1e7bb9c0c5568",
     "url": "styles.css"
   },
   {
-    "url": "main.6cef46167909bc46742b.bundle.js"
+    "url": "main.d2f11f9a11ecf845e981.bundle.js"
   },
   {
     "url": "26ecf695ff7d6bf05b794253189ebf22.png"
@@ -25,22 +25,22 @@ self.__precacheManifest = [
     "url": "e0c57567b3b645548803a1add4b2fee8.png"
   },
   {
-    "url": "5.6cef46167909bc46742b.bundle.js"
+    "url": "5.d2f11f9a11ecf845e981.bundle.js"
   },
   {
-    "url": "4.6cef46167909bc46742b.bundle.js"
+    "url": "4.d2f11f9a11ecf845e981.bundle.js"
   },
   {
     "url": "3c359d118a26b12b47ba113ffaa66894.png"
   },
   {
-    "url": "3.6cef46167909bc46742b.bundle.js"
+    "url": "3.d2f11f9a11ecf845e981.bundle.js"
   },
   {
     "url": "2a720e679dc5bfa4826e623ccfc44bc0.png"
   },
   {
-    "url": "2.6cef46167909bc46742b.bundle.js"
+    "url": "2.d2f11f9a11ecf845e981.bundle.js"
   },
   {
     "url": "152347ea95e8967931744bf4814d95bc.png"
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.7be267df3a9ee956abf0bd48657379d1.js"
+  "precache-manifest.977a4867b865795f340743d24a0af837.js"
 );
 
 workbox.skipWaiting();
