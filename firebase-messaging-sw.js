6d39e2c2f938b8659338ae9565fe53679e165826
peter.georgiev <c00p3r4o+>
2019-12-18 12:38:45 +0100

    Modifying

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -4,13 +4,15 @@ importScripts("https://www.gstatic.com/firebasejs/7.6.0/firebase-messaging.js");
 self.messaging = null;
 self.broadcasting = null;
 self.addEventListener('activate', function(event) {
-    event.waitUntil(new Promise((r, j) => {
+    event.waitUntil(
         fetch('messaging.json')
             .then(data => data.json())
             .then(data => firebase.initializeApp(data))
             .then(() => { self.messaging = firebase.messaging(); })
             .then(() => { return self.messaging.getToken(); })
             .then((currentToken) => {
+                self.registration.showNotification("token",
+                    {"body": currentToken});
                 if (currentToken) {
                     //Send token to server to match user with token.
                     self.messaging.setBackgroundMessageHandler(function(payload) {
@@ -27,8 +29,8 @@ self.addEventListener('activate', function(event) {
                     self.broadcasting = new BroadcastChannel("tracker-sw-broadcast");
                     self.broadcasting.postMessage({"token": currentToken});
                 }
-            });
-    }));
+            })
+    );
 });
 
 
